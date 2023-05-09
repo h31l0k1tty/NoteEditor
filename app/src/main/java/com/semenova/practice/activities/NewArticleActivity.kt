61885da1c864e1b12166dfbe54f39be5b99bc384
Helltraitor <helltraitor@hotmail.com>
2023-05-09 22:53:45 +0300

    Fixed crash on exit from article activity

--- app/src/main/java/com/semenova/practice/activities/NewArticleActivity.kt
+++ app/src/main/java/com/semenova/practice/activities/NewArticleActivity.kt
@@ -1,12 +1,18 @@
 package com.semenova.practice.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.semenova.practice.R
 
 class NewArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_article)
+    }
+
+    fun toAllArticles(view: View){
+        startActivity(Intent(this, AllArticle::class.java))
     }
 }
