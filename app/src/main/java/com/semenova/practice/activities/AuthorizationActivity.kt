3168c3c6a5ddac56cfd14bc6d5938c31bbb4bd66
Helltraitor <helltraitor@hotmail.com>
2023-05-09 20:56:19 +0300

    Post update

--- app/src/main/java/com/semenova/practice/activities/AuthorizationActivity.kt
+++ app/src/main/java/com/semenova/practice/activities/AuthorizationActivity.kt
@@ -74,7 +74,6 @@ class AuthorizationActivity : AppCompatActivity() {
                             editor.putString("password", "")
                             editor.commit()
                         }
-
                     },
                     { error ->
                         var errorMessage: String = ""
