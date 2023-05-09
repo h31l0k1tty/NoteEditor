15cde9a6a3d8cdd27ee09237b475719e67544204
Helltraitor <helltraitor@hotmail.com>
2023-05-10 00:07:48 +0300

    Fixed ignoring picture getting

--- app/src/main/java/com/semenova/practice/activities/NewArticleActivity.kt
+++ app/src/main/java/com/semenova/practice/activities/NewArticleActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.semenova.practice.R
 
 class NewArticleActivity : AppCompatActivity() {
@@ -14,5 +16,13 @@ class NewArticleActivity : AppCompatActivity() {
 
     fun toAllArticles(view: View){
         startActivity(Intent(this, AllArticle::class.java))
+    }
+
+    private val setPicture = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        findViewById<ImageView>(R.id.imageView).setImageURI(it)
+    }
+
+    fun getPicture(view: View) {
+        setPicture.launch("image/*")
     }
 }
